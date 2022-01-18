9d3affaba4742adc3dfb793cfb4b0e44da90d6d8
SALEEM <87938904+Saleem95@users.noreply.github.com>
2022-01-18 22:32:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,17 +5,38 @@ import {useState} from 'react'
 import List from './Components/List';
 
 function App() {
-
-     const [inputlist ,setinputlist] = useState('');
+    const [code ,setcode] = useState('');
+    const [name ,setname] = useState('');
+    const [address ,setaddress] = useState('');
+    const [city ,setcity] = useState('');
+    const [pincode ,setpincode] = useState('');
+    const [statecode ,setstatecode] = useState('');
+    const [gst ,setgst] = useState('');
+    const [contactname ,setcontactname] = useState('');
+    const [conatctnumber ,setconatctnumber] = useState('');
+    const [mobilenumber ,setmobilenumber] = useState('');
+    const [email ,setemail] = useState('');
+    const [comapnycode ,setcomapnycode] = useState('');
     const[Items, setItems] = useState([])
-    const itemChange = e =>{
-      setinputlist(e.target.value);
-    }
+
+    
     const itemSubmit = e =>{
       setItems((data) =>{
-        return [...data,inputlist]
+        return [...data,code,name,address,city,pincode,statecode,gst,contactname,conatctnumber,mobilenumber,email,comapnycode]
       });
-      setinputlist("");
+      // setinputlist("");
+      setcode("");
+      setname("");
+      setaddress();
+      setcity("");
+      setpincode("");
+      setstatecode("");
+      setgst("");
+      setconatctnumber("")
+      setcontactname("");
+      setmobilenumber("");
+      setemail("");
+      setcomapnycode("")
     }
 
     const deleteitem = (id)=>{
@@ -28,23 +49,37 @@ function App() {
       })
   }
     return ( 
-      
+      <>
+      <h1>Add</h1>
      <div className='main'>
-       <h2>Add Deatils</h2>
-        <input onChange={itemChange} type="text" name="input" id="" value={inputlist}/> <br/><br/>
-
+        <div className='inmain'>
+        <div>
+        <input onChange={e=>{setcode(e.target.value)}} type="text" name="code" id="" placeholder='Code' value={code}/> <br/><br/>
+        <input onChange={e=>{setname(e.target.value)}} type="text" name="name" id=""placeholder='Name' value={name}/> <br/><br/>
+        <input onChange={e=>{setaddress(e.target.value)}} type="text" name="address"placeholder='Address' id="" value={address}/> <br/><br/>
+        <input onChange={e=>{setcity(e.target.value)}} type="text" name="city" id=""placeholder='City' value={city}/> <br/><br/>
+        <input onChange={e=>{setpincode(e.target.value)}} type="number" name="pincode"placeholder='Pincode' id="" value={pincode}/> <br/><br/>
+        <input onChange={e=>{setstatecode(e.target.value)}} type="number" name="statecode"placeholder='Statecode' id="" value={statecode}/> <br/><br/> 
+        </div>
+        <div>
+        <input onChange={e=>{setgst(e.target.value)}} type="text" name="gst" id=""placeholder='Gst' value={gst}/> <br/><br/>
+        <input onChange={e=>{setcontactname(e.target.value)}} type="text" name="contactName" id=""placeholder='Contactname' value={contactname}/> <br/><br/>
+        <input onChange={e=>{setconatctnumber(e.target.value)}} type="number" name="conatctnumber"placeholder='Conatctnumber' id="" value={conatctnumber}/> <br/><br/>
+        <input onChange={e=>{setmobilenumber(e.target.value)}} type="number" name="mobilenumber" id=""placeholder='Mobilenumber' value={mobilenumber}/> <br/><br/>
+        <input onChange={e=>{setemail(e.target.value)}} type="email" name="email" id=""placeholder='Email' value={email}/> <br/><br/>
+        <input onChange={e=>{setcomapnycode(e.target.value)}} type="text" name="CompanyCode" id=""placeholder='Comapnycode' value={comapnycode}/> <br/><br/> 
+        </div> 
+        </div>
         <button onClick={itemSubmit} > Add</button>
       
       <ol>
         {Items.map((itemvalue ,index) =>{
-        return <List key={index} id={index} text={itemvalue}
-             onSelect ={deleteitem}
-        />
+        return <List key={index} id={index} text={itemvalue} onSelect ={deleteitem}/>
 
         })}
       </ol>
      </div>
-      
+     </>
      );
 
 }
